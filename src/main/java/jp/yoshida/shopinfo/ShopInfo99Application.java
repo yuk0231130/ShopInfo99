916b48027fd21b804961f34fe8dec181a12f1b50
Yuki Ogasawara <231130@st.yoshida-g.ac.jp>
2023-07-25 11:14:25 +0900

    first commit

---
+++ src/main/java/jp/yoshida/shopinfo/ShopInfo99Application.java
@@ -0,0 +1,13 @@
+package jp.yoshida.shopinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopInfo99Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopInfo99Application.class, args);
+	}
+
+}
---
+++ src/test/java/jp/yoshida/shopinfo/ShopInfo99ApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.yoshida.shopinfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopInfo99ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
